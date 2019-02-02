b39828b2d96a2f80fd32699e154baf2a098c69a5
David Antunes <dvdantunes@gmail.com>
2019-02-02 20:52:51 -0300

    [feat] Add bootstrap 4 to layout and views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,8 @@
 // This is a manifest file that'll be compiled into application.js, which will
-// include all the files
-// listed below.
+// include all the files listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts,
-// vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here
+// vendor/assets/javascripts, or any plugin's vendor/assets/javascripts directory can be referenced here
 // using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at
@@ -15,20 +13,10 @@
 // https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require bootstrap/affix
-//= require bootstrap/alert
-//= require bootstrap/button
-//= require bootstrap/carousel
-//= require bootstrap/collapse
-//= require bootstrap/dropdown
-//= require bootstrap/modal
-//= require bootstrap/tooltip
-//= require bootstrap/popover
-//= require bootstrap/scrollspy
-//= require bootstrap/tab
-//= require bootstrap/transition
+//= require popper
+//= require bootstrap
 //= require_tree .
