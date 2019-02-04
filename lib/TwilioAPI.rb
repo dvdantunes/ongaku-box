7c46139f61786c9124081edf241d6fe514c4bef2
David Antunes <dvdantunes@gmail.com>
2019-02-04 05:05:23 -0300

    [code] Add Twilio API wrapper

---
+++ lib/TwilioAPI.rb
@@ -0,0 +1,62 @@
+require 'twilio-ruby'
+
+#
+# Twilio API wrapper
+#
+class TwilioAPI
+
+  ACCOUNT_SID = 'ACbbe2dc3ba80ab057cd8001d6246cb6fd'
+  AUTH_TOKEN  = 'c1c90f221e5b70e9e993de0e752e091e'
+  TWILIO_FROM_NUMBER = '+14436817535'
+
+
+  def initialize
+    @client = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
+  end
+
+
+  #
+  # Send a SMS message to the specified number
+  #
+  # @return [boolean]    If message was sent or not
+  def send_sms(body, to_number)
+
+    begin
+      message = @client.messages.create({
+        :from => TWILIO_FROM_NUMBER,
+        :to   => to_number,
+        :body => body
+      })
+
+    rescue Twilio::REST::RestError => e
+      return false
+    end
+
+    return is_sent message
+  end
+
+
+  #
+  # Return the list of SMS already sent
+  #
+  # @return [Array]
+  def list_sms
+    @client.api.account.messages.list
+  end
+
+
+
+  private
+
+
+  #
+  # Determine if a message was sent or not
+  #
+  # @return boolean
+  def is_sent(message)
+    message.sid.present? &&
+      (message.status == 'delivered' ||
+        message.error_code.zero?)
+  end
+
+end
