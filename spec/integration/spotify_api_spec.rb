1cd0f3be030d9b0ba5172a5ef7119afddd776e17
David Antunes <803679+dvdantunes@users.noreply.github.com>
2019-02-07 05:39:39 -0300

    [test] Improvements on integration specs

--- spec/integration/spotify_api_spec.rb
+++ spec/integration/spotify_api_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 require 'SpotifyAPI'
 
-RSpec.describe 'Spotify API wrapper' do
-  let(:spotify) { SpotifyAPI.new }
+RSpec.describe SpotifyAPI do
+  subject { described_class.new }
   let(:existing_artist) { Faker::Music.band }
   let(:fake_artist) { 'blink183' }
 
@@ -10,14 +10,14 @@ RSpec.describe 'Spotify API wrapper' do
 
     context 'when a existing artist is provided' do
       it 'successfully gets its top track' do
-        top_track = spotify.get_artist_top_track(existing_artist)
+        top_track = subject.get_artist_top_track(existing_artist)
         expect(top_track).to be
       end
     end
 
     context 'when a fake artist is provided' do
       it 'doesn\'t get anything' do
-        top_track = spotify.get_artist_top_track(fake_artist)
+        top_track = subject.get_artist_top_track(fake_artist)
         expect(top_track).to be_nil
       end
     end
