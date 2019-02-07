e926aa2c472696159dba30831c24fcf9ef555007
David Antunes <803679+dvdantunes@users.noreply.github.com>
2019-02-07 02:31:35 -0300

    [fix][test] Fix Twilio and Spotify API libs according to updates on tests specs

--- lib/SpotifyAPI.rb
+++ lib/SpotifyAPI.rb
@@ -8,6 +8,7 @@ class SpotifyAPI
   CLIENT_ID     = '5cae77f4d2ed4693bc8516e296a73986'
   CLIENT_SECRET = '4b0fdfd6e09b4c4d999a0f1bd3fc35e8'
 
+
   # Initialization and authentication
   def initialize
     RSpotify::authenticate(CLIENT_ID, CLIENT_SECRET)
@@ -24,8 +25,11 @@ class SpotifyAPI
   # @return [String|nil]          Top track found or 'nil' if it didn't found anything
   def get_artist_top_track(artist_name, country = :US)
     artist = RSpotify::Artist.search(artist_name).first
-    return nil unless artist.present?
 
-    artist.top_tracks(country).first
+    return nil unless artist.present?
+    return nil unless artist.top_tracks(country).first.present?
+
+    artist.top_tracks(country).first.name
   end
+
 end
--- spec/integration/twilio_api_spec.rb
+++ spec/integration/twilio_api_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 require 'TwilioAPI'
 
-describe "Twilio API wrapper" do
+RSpec.describe 'Twilio API wrapper' do
   let(:twilio) { TwilioAPI.new }
   let(:good_phone) { '+56952496480' }
   let(:bad_phone) { '7480' }
   let(:body) { 'test sms'}
 
-  context "#send_sms" do
+  context '#send_sms' do
 
-    context 'when a well-formatted phone is specified' do
-      it "successfully sends a message" do
+    context 'when a well-formatted phone is provided' do
+      it 'successfully sends a SMS', skip: true do
         sent = twilio.send_sms(body, good_phone)
         expect(sent).to be
       end
     end
 
-    context 'when a bad-formatted phone is specified' do
-      it "doesn't send a message" do
+    context 'when a bad-formatted phone is provided' do
+      it 'doesn\'t send a SMS' do
         sent = twilio.send_sms(body, bad_phone)
         expect(sent).to be_falsey
       end
