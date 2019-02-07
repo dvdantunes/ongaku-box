f319dfe81564345d2d08f8624117d433eb529b36
David Antunes <803679+dvdantunes@users.noreply.github.com>
2019-02-07 05:39:06 -0300

    [test] Fix FactoryBot configuration

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -5,4 +5,8 @@ require 'factory_bot'
 # FactoryBot configuration
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryBot.find_definitions
+  end
 end
