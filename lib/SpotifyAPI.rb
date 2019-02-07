09349b7a1b12993410abd908af8487aba7559774
David Antunes <803679+dvdantunes@users.noreply.github.com>
2019-02-07 00:56:48 -0300

    [feat][test] Add spotify API wrapper and its test specs

---
+++ lib/SpotifyAPI.rb
@@ -0,0 +1,31 @@
+require 'rspotify'
+
+#
+# Spotify API wrapper
+#
+class SpotifyAPI
+
+  CLIENT_ID     = '5cae77f4d2ed4693bc8516e296a73986'
+  CLIENT_SECRET = '4b0fdfd6e09b4c4d999a0f1bd3fc35e8'
+
+  # Initialization and authentication
+  def initialize
+    RSpotify::authenticate(CLIENT_ID, CLIENT_SECRET)
+  end
+
+
+  # Get and artist top track
+  #
+  # Perform a search request by provided artis name
+  #
+  # @param [String] artist_name   Artist name
+  # @param [Symbol] country       [Optional] In which country to find top tracks
+  #
+  # @return [String|nil]          Top track found or 'nil' if it didn't found anything
+  def get_artist_top_track(artist_name, country = :US)
+    artist = RSpotify::Artist.search(artist_name).first
+    return nil unless artist.present?
+
+    artist.top_tracks(country).first
+  end
+end
---
+++ spec/integration/spotify_api_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+require 'SpotifyAPI'
+
+describe "Spotify API wrapper" do
+  let(:spotify) { SpotifyAPI.new }
+  let(:existing_artist) { Faker::Music.band }
+  let(:fake_artist) { 'blink183' }
+
+
+  context "#get_artist_top_track" do
+
+    context 'when a existing artist is provided' do
+      it "successfully gets its top track" do
+        top_track = spotify.get_artist_top_track(existing_artist)
+        expect(top_track).to be
+      end
+    end
+
+    context 'when a fake artist  is provided' do
+      it "doesn't get anything" do
+        top_track = spotify.get_artist_top_track(fake_artist)
+        expect(top_track).to be_nil
+      end
+    end
+  end
+
+end
