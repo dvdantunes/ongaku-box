2456fafec1041f3cdd07433bfade16f86b85e8b2
David Antunes <dvdantunes@gmail.com>
2019-02-07 05:39:39 -0300

    [test] Improvements on integration specs

--- spec/integration/twilio_api_spec.rb
+++ spec/integration/twilio_api_spec.rb
@@ -1,24 +1,24 @@
 require 'rails_helper'
 require 'TwilioAPI'
 
-RSpec.describe 'Twilio API wrapper' do
-  let(:twilio) { TwilioAPI.new }
+RSpec.describe TwilioAPI do
+  subject { described_class.new }
   let(:good_phone) { '+56952496480' }
   let(:bad_phone) { '7480' }
-  let(:body) { 'test sms'}
+  let(:body) { 'test sms' }
 
   context '#send_sms' do
 
     context 'when a well-formatted phone is provided' do
-      it 'successfully sends a SMS', skip: true do
-        sent = twilio.send_sms(body, good_phone)
+      it 'successfully sends a SMS' do
+        sent = subject.send_sms(body, good_phone)
         expect(sent).to be
       end
     end
 
     context 'when a bad-formatted phone is provided' do
       it 'doesn\'t send a SMS' do
-        sent = twilio.send_sms(body, bad_phone)
+        sent = subject.send_sms(body, bad_phone)
         expect(sent).to be_falsey
       end
     end
