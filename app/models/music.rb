9834b5e9e0c77f199d7ca55e5c7de9dde4687c3b
David Antunes <dvdantunes@gmail.com>
2019-02-07 02:32:32 -0300

    [feat][test] Add music model and its test spec

---
+++ app/models/music.rb
@@ -0,0 +1,43 @@
+#
+# Music model
+#
+class Music
+  include ActiveModel::Model
+
+  # Sends a SMS to provided number with the top track of
+  # provided artist
+  #
+  # @param [String] artist_name     Artist name
+  # @param [String] phone_number    Phone number in international format (eg: +14436817535)
+  #
+  # @return [Boolean]     True if SMS message was sent
+  def send_artist_top_track(artist_name, phone_number)
+    top_track = nil
+    sent_message = nil
+
+    # Perform a search of artist's top track
+    begin
+      spotify = SpotifyAPI.new
+      top_track = spotify.get_artist_top_track(artist_name)
+
+    rescue Exception => e
+      Rails.logger.error 'Spotify error: cannot connect or use API'
+    end
+
+    # Abort sending if nothing found
+    return false unless top_track.present?
+
+    # Send a SMS to the provided number
+    begin
+      twilio = TwilioAPI.new
+
+      body = "#{artist_name} top track is: #{top_track}"
+      sent_message = twilio.send_sms(body, phone_number)
+
+    rescue Exception => e
+      Rails.logger.error 'Twilio error: cannot connect or use API'
+    end
+
+    sent_message
+  end
+end
