01d94d2345d0a7f3f4ec18787887fa61438b9af2
David Antunes <dvdantunes@gmail.com>
2019-02-12 04:00:45 -0300

    [feat] Add plain layout and view for home/index action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
+#
+# Home controller
+#
 class HomeController < ApplicationController
+
+  # Displays a form to allow users to search for
+  # an artist top track and request to send it to her/his phone
+  #
+  # @return [void]
   def index
+
   end
 end
