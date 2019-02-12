0508bd6a471fb0c3fba6585582463e0caa3ddd8d
David Antunes <803679+dvdantunes@users.noreply.github.com>
2019-02-12 04:00:45 -0300

    [feat] Add plain layout and view for home/index action

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,4 +1,11 @@
-
 /**
  * Home controller related logic, behaviors and hooks
  */
+
+$(function () {
+  'use strict'
+
+  $('[data-toggle="offcanvas"]').on('click', function () {
+    $('.offcanvas-collapse').toggleClass('open')
+  })
+})
